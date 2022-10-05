2df164dc30b0b175903e22bab74d9140cb7c9635
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-10-05 08:22:55 -0600

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,22 @@
+import pygame
+import time
+pygame.init()
+pygame.display.set_caption("data structures")
+screenSize = (800, 800)
+screen = pygame.display.set_mode(screenSize) #creates the obvious game screen
+
+#names
+FRIEND1 = ("Lukas") #THIS IS A TUPLE!!!
+FRIEND2 = ("Ricardo")
+FRIEND3 = ("Xitlali")
+FRIEND4 = ("Jesse")
+FRIEND5 = ("Rey")
+#Tuples cannot be changed once set up!
+
+print(FRIEND1)
+print(FRIEND2)
+print(FRIEND3)
+print(FRIEND4)
+print(FRIEND5)
+
+print(FRIEND3)
