fbeb5e765fdc1300e73e7af18f602a4e368a4cd2
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-10-05 09:22:34 -0600

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+
+#THIS IS A DICTIONARY!!!
+snarf = {"Single": "5.95",
+         "Hot chicken sandwich": "8.20",
+         "Milkshake": "5.75",
+         "Grilled Cheese": "3.25",
+         "French Fries": "3.05"}
+
+#anything before the : is a key while anything after the : is a value!!!
+
+print(snarf) #prints the whole dictionary!
+
